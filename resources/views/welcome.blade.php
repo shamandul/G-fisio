d6ec9fcf1b4a2faf879ccc6dd8ef746650ac5ed4
Jesús de Serdio <shamandul@gmail.com>
2018-03-28 11:17:32 +0200

    mejora en la vista welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,45 +12,48 @@
       </div>
     </div>
   </div>
-  <div class="texto">
-    <h1>Ejemplo paralax</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  </div>
+  <div class="container">
+    <div class="texto">
+      <h1>Ejemplo paralax</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+    </div>
+</div>
   <div class="contenedor">
-  <div id="seccion1">
+    <div id="seccion1">
+    </div>
   </div>
-  </div>
-
-  <div class="texto">
-  <h1>Ejemplo relax</h1>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
-  </div>
+  <div class="container">
+    <div class="texto">
+      <h1>Ejemplo relax</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure, reprehenderit quos quaerat dicta nesciunt tenetur aliquam! Aliquid unde, vero, laboriosam accusamus voluptas totam nesciunt assumenda quae voluptatibus, dolor sunt.</p>
+    </div>
+</div>
 @endsection
 
 @section('script')

68f5b5dafdc05dda6f0ed83dacb3fef7798f110f
Jesús de Serdio <shamandul@gmail.com>
2018-04-15 13:17:36 +0200

    Actualización del borrado en citas

--- app/Http/Controllers/CitasController.php
+++ app/Http/Controllers/CitasController.php
@@ -96,6 +96,8 @@ class CitasController extends Controller
      */
     public function destroy($id)
     {
+        DB::table('facturas')
+        ->where('id_citas', '$id')->delete();
         $cita = Cita::findOrFail($id);
         $cita->delete();
     }
